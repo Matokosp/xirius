9fc03ddbe197b56169d14a384428be68d483b74a
Matokosp <jmsalinas@uc.cl>
2020-08-04 21:17:07 -0400

    cursor

---
+++ assets/js/myscript.js
@@ -0,0 +1,31 @@
+var md = window.matchMedia( "(min-width: 1000px)" );
+
+  if (md.matches) {
+
+		var i = null;
+
+    $(document).on('mousemove', function(e) {
+
+			var x = e.clientX;
+			var y = e.clientY;
+
+      $('#cursor').css({
+        left: e.clientX,
+        top: e.clientY,
+      });
+
+			$("#cursorRound").show();
+			$('#cursorRound').css({
+				opacity: '1',
+        left: e.clientX - 28,
+        top: e.clientY - 28,
+				width: '20px',
+				height: '20px'
+      });
+
+			// i = setTimeout('$("#cursorRound").css({width:"0",height:"0",opacity: "0"});', 1000);
+
+    }).mouseleave(function() {
+				clearTimeout(i);
+		});
+    }
