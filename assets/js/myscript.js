345060c7d071055f7e5b39664bea6409ce9aff38
Matokosp <jmsalinas@uc.cl>
2020-08-16 15:38:49 -0400

    category page update

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,95 +1,85 @@
 // SCROLL WINDOW TO ID
 
-$('a[href^="#"]').on('click', function(event) {
-		var target = $(this.getAttribute('href'));
-		if( target.length ) {
-				event.preventDefault();
-				$('html, body').stop().animate({
-						scrollTop: target.offset().top
-				}, 1000);
-		}
+$('a[href^="#"]').on("click", function (event) {
+  var target = $(this.getAttribute("href"));
+  if (target.length) {
+    event.preventDefault();
+    $("html, body").stop().animate(
+      {
+        scrollTop: target.offset().top,
+      },
+      1000
+    );
+  }
 });
 
-var md = window.matchMedia( "(min-width: 1100px)" );
+var md = window.matchMedia("(min-width: 1100px)");
 
-  if (md.matches) {
-
-    $(document).ready(function(){
-      // $("#cursorRound").show();
-      $('#cursorRound').css({
-        width: '24px',
-				height: '24px'
-      })
+if (md.matches) {
+  $(document).ready(function () {
+    // $("#cursorRound").show();
+    $("#cursorRound").css({
+      width: "24px",
+      height: "24px",
     });
+  });
 
-		var i = null;
+  var i = null;
 
-    $(document).on('mousemove', function(e) {
+  $(document)
+    .on("mousemove", function (e) {
+      var x = e.clientX;
+      var y = e.clientY;
 
-			var x = e.clientX;
-			var y = e.clientY;
-
-      $('#cursor').css({
+      $("#cursor").css({
         left: e.clientX,
         top: e.clientY,
       });
 
-
-			$('#cursorRound').css({
-				opacity: '1',
+      $("#cursorRound").css({
+        opacity: "1",
         left: e.clientX - 8,
-        top: e.clientY - 8
+        top: e.clientY - 8,
       });
-
-
-    }).mouseleave(function() {
-				clearTimeout(i);
-		});
-
-    $('.category').on('mouseenter', function(){
-      $('#cursorRound').css({
-        transform: "scale(4)",
-        border: "transparent",
-        background: "rgba(255, 143, 148, 0.7)",
-      })
     })
-    $('.category').on('mouseleave', function(){
-      $('#cursorRound').css({
-        transform: "scale(1)",
-        border: "solid 2px #ff8f94",
-        background: "transparent"
-      })
-    })
+    .mouseleave(function () {
+      clearTimeout(i);
+    });
 
-    }
+  $(".category").on("mouseenter", function () {
+    $("#cursorRound").css({
+      transform: "scale(4)",
+      border: "transparent",
+      background: "rgba(255, 143, 148, 0.7)",
+    });
+  });
+  $(".category").on("mouseleave", function () {
+    $("#cursorRound").css({
+      transform: "scale(1)",
+      border: "solid 2px #ff8f94",
+      background: "transparent",
+    });
+  });
+}
 
+// HOME ABOUT TITLES
 
-		// HOME ABOUT TITLES
-
-		$( document ).ready(function() {
-		   $('#prismaText').hide();
-		});
-
-		$('#aboutButt').on('click', function(){
-			$('#aboutText').show();
-			$('#prismaText').hide();
-			$('#aboutButt').addClass('home_about_title-active');
-			$('#prismaButt').removeClass('home_about_title-active');
-		})
-
-		$('#prismaButt').on('click', function(){
-			$('#prismaText').show();
-			$('#aboutText').hide();
-			$('#aboutButt').removeClass('home_about_title-active');
-			$('#prismaButt').addClass('home_about_title-active');
-		})
-
-		// SLICK JS
-
-
-$('.team_slider').slick({
-	dots: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1
+$(document).ready(function () {
+  $("#prismaText").hide();
 });
+
+$("#aboutButt").on("click", function () {
+  $("#aboutText").show();
+  $("#prismaText").hide();
+  $("#aboutButt").addClass("home_about_title-active");
+  $("#prismaButt").removeClass("home_about_title-active");
+});
+
+$("#prismaButt").on("click", function () {
+  $("#prismaText").show();
+  $("#aboutText").hide();
+  $("#aboutButt").removeClass("home_about_title-active");
+  $("#prismaButt").addClass("home_about_title-active");
+});
+
+// SLICK JS
